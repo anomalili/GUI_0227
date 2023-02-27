228865f38e3b78e348d5c8ee3eda4522a484adc0
mzsl <95681537+anomalili@users.noreply.github.com>
2023-02-27 10:04:32 +0100

    modell jav.

--- src/model/emberekModel.java
+++ src/model/emberekModel.java
@@ -10,11 +10,48 @@ package model;
  */
 public class emberekModel {
 
-    public emberekModel(String nev, int kor, String neme, int MunkToltEv) {
+public emberekModel(String nev, int kor, String neme, int MunkToltEv) {
         this.nev = nev;
         this.kor = kor;
         this.neme = neme;
         this.MunkToltEv = MunkToltEv;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public String getNeme() {
+        return neme;
+    }
+
+    public int getMunkToltEv() {
+        return MunkToltEv;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+
+    public void setNeme(String neme) {
+        this.neme = neme;
+    }
+
+    public void setMunkToltEv(int MunkToltEv) {
+        this.MunkToltEv = MunkToltEv;
+    }
+
+    @Override
+    public String toString() {
+        return "emberekModel{" + "nev=" + nev + ", kor=" + kor + ", neme=" + neme + ", MunkToltEv=" + MunkToltEv + '}';
     }
     public String nev;
     public int kor;
